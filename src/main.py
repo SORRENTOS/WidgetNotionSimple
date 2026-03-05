d1f305085d569a72c6d9c859a1f1b3ca69458849
SORRENTOS <106037109+SORRENTOS@users.noreply.github.com>
2026-03-04 22:29:42 -0300

    tu puedes papito

--- src/main.py
+++ src/main.py
@@ -6,13 +6,20 @@ def main(page: ft.Page):
 
 
 
+    boton1 = ft.ElevatedButton("Outlook", bgcolor=ft.Colors.BLUE,width=150,height=50,icon= ft.Icons.EMAIL_ROUNDED,style= ft.ButtonStyle(
+        shape= ft.RoundedRectangleBorder(radius=10)),url="https://outlook.office.com/mail/")
+    
+    boton3 = ft.ElevatedButton("\U0001F4D5 BANNER", bgcolor=ft.Colors.RED,width=150,height=50,style= ft.ButtonStyle(
+        shape= ft.RoundedRectangleBorder(radius=10)),url="https://uacloud.uautonoma.cl/dashboard")
 
 
 
+    
+    fila_botones = ft.Row([boton1,boton3],spacing=100)
     page.add(
         ft.SafeArea(
-            ft.Text("hola"),
-            
+            fila_botones,
+            expand=True,
         )
     )
 
