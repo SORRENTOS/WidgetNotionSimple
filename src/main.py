48fbb82f6ce9fcf048ee270d1fe41c871fbd8fb6
SORRENTOS <106037109+SORRENTOS@users.noreply.github.com>
2026-03-05 21:19:27 -0300

    responsive update

--- src/main.py
+++ src/main.py
@@ -11,15 +11,15 @@ def main(page: ft.Page):
     
     boton3 = ft.ElevatedButton("\U0001F4D5 BANNER", bgcolor=ft.Colors.RED,width=150,height=50,style= ft.ButtonStyle(
         shape= ft.RoundedRectangleBorder(radius=10)),url="https://uacloud.uautonoma.cl/dashboard")
-    Hora_texto = ft.Text(size=30,weight=ft.FontWeight.BOLD,expand=True,text_align=ft.TextAlign.CENTER) 
-    Minuto_texto = ft.Text(size=30,weight=ft.FontWeight.BOLD,expand=True,text_align=ft.TextAlign.CENTER) 
-    Segundos_texto = ft.Text(size=30,weight=ft.FontWeight.BOLD,expand=True,text_align=ft.TextAlign.CENTER) 
+    Hora_texto = ft.Text(size=30,weight=ft.FontWeight.BOLD,text_align=ft.TextAlign.CENTER) 
+    Minuto_texto = ft.Text(size=30,weight=ft.FontWeight.BOLD,text_align=ft.TextAlign.CENTER) 
+    Segundos_texto = ft.Text(size=30,weight=ft.FontWeight.BOLD,text_align=ft.TextAlign.CENTER) 
 
     container_hora = ft.Container(content=Hora_texto,width=45,height=50,bgcolor=ft.Colors.GREY_900,border_radius=10)
     container_Minutos = ft.Container(content=Minuto_texto,width=45,height=50,bgcolor=ft.Colors.GREY_900,border_radius=10)
     container_Segundos = ft.Container(content=Segundos_texto,width=45,height=50,bgcolor=ft.Colors.GREY_900,border_radius=10)
-    fila_tiempo = ft.Row([container_hora,ft.Text(":",weight=ft.FontWeight.BOLD,size=30,expand=True,text_align=ft.TextAlign.CENTER),container_Minutos,ft.Text(":",weight=ft.FontWeight.BOLD,size=30),container_Segundos],spacing=1)
-    container_reloj = ft.Container(content=fila_tiempo,bgcolor=ft.Colors.GREY_800,padding=5,border_radius=10)
+    fila_tiempo = ft.Row([container_hora,ft.Text(":",weight=ft.FontWeight.BOLD,size=30,text_align=ft.TextAlign.CENTER),container_Minutos,ft.Text(":",weight=ft.FontWeight.BOLD,size=30),container_Segundos],spacing=1)
+    container_reloj = ft.Container(content=fila_tiempo,bgcolor=ft.Colors.GREY_800,padding=5,border_radius=10,width=170)
 
     texto_clase_actual = ft.Text(value="CLASE",size=15,weight=ft.FontWeight.BOLD,expand=True,text_align=ft.TextAlign.CENTER)
     container_Clase_actual = ft.Container(content=texto_clase_actual,height=60,width=120,bgcolor=ft.Colors.GREY_900,border_radius=10,alignment=ft.Alignment.CENTER)
@@ -96,7 +96,7 @@ def main(page: ft.Page):
             await asyncio.sleep(1)
 
     
-    fila_botones = ft.Row([boton1,boton3,container_reloj,columan_clase_actual],vertical_alignment=ft.MainAxisAlignment.CENTER,spacing=50)
+    fila_botones = ft.Row([boton1,boton3,container_reloj,columan_clase_actual],vertical_alignment=ft.MainAxisAlignment.CENTER,spacing=50,wrap=True)
     page.add(
         ft.SafeArea(
             fila_botones,
